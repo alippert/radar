cce62cbe092037f5637b4368b8414ff1f796958b
Andrew Lippert <andrew@ednovate.org>
2012-10-25 16:17:54 -0700

    Initial Commit
    USC Hybrid High School RADAR - a Rails 3.2 application for managing the
    progress and performance of USCHHS students.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radar::Application.config.secret_token = 'a9054516336cddd96865af02a1774a7c60de2e93643d48f168c0eabfde13878291475270ab5c3bf595ef7b84fa8bb6c1030871e9bbc1f36d2c65e82e679a8f01'
