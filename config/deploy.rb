2c4ffe24bfaf5ed3ad799b7108bf5e67af4ab439
Andrew Lippert <alippert@mac.com>
2012-12-04 01:38:04 -0800

    Add username to production db connection.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,13 @@ set :scm_username, 'alippert'
 set :repository, 'https://github.com/alippert/radar.git'
 
 # rvm configuration
-set :rvm_ruby_string, '1.9.3-p327@radar'
-require 'rvm/capistrano'
-set :rvm_type, :system
+set :rvm_type, :system    # :user is the default
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
+
+require "rvm/capistrano"  # Load RVM's capistrano plugin.
+
+# We use sudo (root) for system-wide RVM installation
+set :rvm_install_with_sudo, true
 
 set :application, 'radar'
 set :deploy_to, "/opt/#{application}"
@@ -21,6 +25,9 @@ ssh_options[:username] = deploy
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 server "apps.uschhs.org", :app, :web, :db, :primary => true
+
+# Apply default RVM version for the current account
+after "deploy:setup", "deploy:set_rvm_version"
 
 # automatically bundle new gems
 require "bundler/capistrano" 
@@ -42,5 +49,34 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
+
+  task :set_rvm_version, :roles => :app, :except => { :no_release => true } do
+    run "source /etc/profile.d/rvm.sh && rvm use #{rvm_ruby_string} --default"
+  end
+
+  task :fix_setup_permissions, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} chgrp #{user_rails} #{shared_path}/log"
+    run "#{sudo} chgrp #{user_rails} #{shared_path}/pids"
+  end
+
+  task :fix_permissions, :roles => :app, :except => { :no_release => true } do
+    # To prevent access errors while moving/deleting
+    run "#{sudo} chmod 775 #{current_path}/log"
+    run "#{sudo} find #{current_path}/log/ -type f -exec chmod 664 {} \\;"
+    run "#{sudo} find #{current_path}/log/ -exec chown #{user}:#{user_rails} {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -type f -exec chmod 664 {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -type d -exec chmod 775 {} \\;"
+    run "#{sudo} find #{current_path}/tmp/ -exec chown #{user}:#{user_rails} {} \\;"
+  end
+
 end
 
+# Helper function
+def remote_file_exists?(full_path)
+  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
+end
